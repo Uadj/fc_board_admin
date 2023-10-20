59c78dba33cfc23ae855511439c11aee63321a2b
dgw <30551889+Uadj@users.noreply.github.com>
2023-10-21 04:02:43 +0900

    Initial commit

---
+++ src/main/java/com/example/fc_board_admin/FcBoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.fc_board_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcBoardAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcBoardAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fc_board_admin/FcBoardAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fc_board_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcBoardAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
